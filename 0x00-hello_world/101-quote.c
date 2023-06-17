5562821d0af82f9e9564bb3880ee0006c86b0590
Mutharimi <dmuthaa@gmail.com>
2023-06-17 13:18:27 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *UNIX is basically a simple operating system
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
