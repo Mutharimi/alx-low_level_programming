44bfcddac7a549adf8617d124256a8a8912fe4bf
Mutharimi <dmuthaa@gmail.com>
2023-06-18 13:08:29 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int digit = 32;
+
+while (digit <= 96)/96;
+{
+putchar(digit);
+
+if (digit == 48)
+digit += 32;
+++digit;
+}
+putchar('\n');
+
+return (0);
+}
+
