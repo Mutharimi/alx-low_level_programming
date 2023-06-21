de956debd5df0b41db7cf153cdd258e025a2e91b
Mutharimi <dmuthaa@gmail.com>
2023-06-21 13:07:24 -0700

    lll

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,9 @@
  * @n: The input number to be checked
  * Return: 1 for positive, 0 for zero, -1 for negative
  */
+
 int print_sign(int n)
+
 {
 if (n > 0)
 {
@@ -23,10 +25,5 @@ _putchar('0');
 return (0);
 }
 }
-int main(void)
-{
-int number = 10;
-int result = print_sign(number);
-return (0);
-}
+
 
