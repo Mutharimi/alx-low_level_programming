37dbd2eaa81f55a0bcc6fd0160f2a8b255e5a808
Mutharimi <dmuthaa@gmail.com>
2023-06-21 09:20:19 -0700

    kk

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,11 @@
  *description: prints the alphabet, in lowercase, followed by a new line
 */
 
-/* funtion to print alphabet*/
-void print_alphabet(void)
+void print_alphabet(void);
 {
-char la;
-for (la = 'a'; la <= 'z'; la++)
-{
-_putchar(la);
-}
-_putchar('\n')
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
