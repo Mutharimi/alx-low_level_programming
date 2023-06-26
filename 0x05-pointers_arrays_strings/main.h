4a7cd69131c8f0ea11c4e9887c37fc315e94523f
Mutharimi <dmuthaa@gmail.com>
2023-06-26 12:49:25 -0700

    changes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,5 +16,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-
 #endif
+
+
