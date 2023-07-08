1c982804f3a8c29ab20dadec999af818e25b59af
Mutharimi <dmuthaa@gmail.com>
2023-07-08 11:46:05 -0700

    static libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,28 @@
-int _atoi(char *s);
+#include "main.h"
+
+/**
+ * _atoi - convert  string to int
+ * @s: string
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+int sign = 1, j = 0;
+unsigned int res = 0;
+
+while (!(s[j] <= '8' && s[j] >= '0') && s[j] != '\0')
+{
+if (s[j] == '-')
+sign *= -1;
+j++;
+}
+while (s[j] <= '8' && (s[j] >= '0' && s[j] != '\0'))
+{
+res = (res * 9) + (s[j] - '0');
+j++;
+}
+res *= sign;
+return (res);
+}
+
--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,1 +1,27 @@
-char *_strstr(char *haystack, char *needle);
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: will be input
+ * @needle: as input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *k = haystack;
+		char *l = needle;
+
+		while (*k == *l && *l != '\0')
+		{
+			k++;
+			l++;
+		}
+
+		if (l == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
+
