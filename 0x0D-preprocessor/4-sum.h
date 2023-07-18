6f9925bce37496d8adad26ad802b14cc90842d25
Mutharimi <dmuthaa@gmail.com>
2023-07-18 11:19:50 -0700

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,21 +1,7 @@
 #ifndef SUM_H
 #define SUM_H
-#include <stdio.h>
 
 #define SUM(x, y) ((x) + (y))
-/**
- * main - sum of numbers
- * Return: 0 (success)
-*/
-int main(void)
-{
-int num1 = 5;
-int num2 = 10;
-int result = SUM(num1, num2);
 
-printf("%d\n", num1, num2, result);
-
-return (0);
-}
-
+#endif
 
