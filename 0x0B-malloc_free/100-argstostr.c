c419be7be33869863a1bd1f03f4010002edf07e8
Mutharimi <dmuthaa@gmail.com>
2023-07-18 09:57:54 -0700

    chamges

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,32 +9,30 @@
  * Return: 0
 */
 
-
 char *argstostr(int ac, char **av)
 {
-if (ac == 0 || av == NULL)
-return (NULL);
+	int i,n,r = 0, int l = 0;
+	char *str;
 
-int total_length = 0;
-for (int i = 0; i < ac; i++)
-{
-total_length += strlen(av[i]) + 1;
-}
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-char *result = (char *)malloc(sizeof(char) * total_length + 1);
-if (result == NULL)
-return (NULL);
+	for (int i = 0; i < ac; i++)
+	{
+		for (int n = 0; av[i][n]; n++)
+			l++;
+	}
+	l += ac;
 
-int index = 0;
-for (int i = 0; i < ac; i++)
-{
-int length = strlen(av[i]);
-memcpy(result + index, av[i], length);
-index += length;
-result[index++] = '\n';
-}
-result[total_length] = '\0';
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+	for (n = 0; av[i][n]; n++)
+	
+	}
 
-return (result);
+	return (str);
 }
 
